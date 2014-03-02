2d340a0e069e380a62aea7b0afa039daedee6cb7
ips1 <nando128@gmail.com>
2014-03-03 00:34:37 +0100

    Initial commit

---
+++ src/cz/cuni/mff/kubatpe1/java/cnen/CNEN.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cuni.mff.kubatpe1.java.cnen;
+
+/**
+ *
+ * @author Petr
+ */
+public class CNEN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
